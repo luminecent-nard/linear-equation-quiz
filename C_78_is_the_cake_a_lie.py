75d075c787a7e4663a9b69a23272f6b218cb82b3
luminecent-nard <stapletonr4257@masseyhigh.school.nz>
2026-05-19 14:58:52 +1200

    C_78_is_the_cake_a_lie.py

--- C_78_is_the_cake_a_lie.py
+++ C_78_is_the_cake_a_lie.py
@@ -1,35 +1,3 @@
-from time import sleep
-
-#main routine
-
-while True:
-
-    cake = "the cake is a lie"
-    answer = input("is the cake a lie? ")
-
-    if answer == "xxx":
-        print("i can't escape, neither can you")
-
-    elif answer == "uwu" or answer == "owo":
-        while True:
-            print("you disgust me")
-            sleep(0.5)
-            print("now perish")
-            sleep(0.5)
-            self_destruct = 0/0
-            print(self_destruct)
-
-    elif answer == "yes":
-        while True:
-            print(cake)
-            try_to_exit = input()
-            if try_to_exit == "xxx":
-                while True:
-                    print("you cant do that")
-
-
-    else:
-        while True:
-            print(cake)
+print("the cake is a lie")
 
 
